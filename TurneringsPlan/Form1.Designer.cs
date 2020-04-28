6dcc3ee799bb248e9b34c2275f4ea85c2416b7cc
Victor Hoffmann Risager <vict2464@elev.aatg.dk>
2020-04-28 14:21:15 +0200

    Added a Form. Filled out the Team Creator form. Added a few lines of code to create a person, and add them to the team listbox. Also added a small name validation function.

---
+++ TurneringsPlan/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace TurneringsPlan
+{
+    partial class formTeamCreator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TeamCreator = new System.Windows.Forms.Label();
+            this.TeamName = new System.Windows.Forms.Label();
+            this.lsbPlayerInTeam = new System.Windows.Forms.ListBox();
+            this.txtTeamName = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.CreatePlayer = new System.Windows.Forms.Label();
+            this.FirstName = new System.Windows.Forms.Label();
+            this.LastName = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.btnCreatePlayer = new System.Windows.Forms.Button();
+            this.btnAddToTeam = new System.Windows.Forms.Button();
+            this.cmbPlayers = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TeamMembers = new System.Windows.Forms.Label();
+            this.ChoosePlayer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TeamCreator
+            // 
+            this.TeamCreator.AutoSize = true;
+            this.TeamCreator.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TeamCreator.Location = new System.Drawing.Point(85, 37);
+            this.TeamCreator.Name = "TeamCreator";
+            this.TeamCreator.Size = new System.Drawing.Size(207, 33);
+            this.TeamCreator.TabIndex = 0;
+            this.TeamCreator.Text = "Team Creator";
+            // 
+            // TeamName
+            // 
+            this.TeamName.AutoSize = true;
+            this.TeamName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TeamName.Location = new System.Drawing.Point(471, 76);
+            this.TeamName.Name = "TeamName";
+            this.TeamName.Size = new System.Drawing.Size(137, 25);
+            this.TeamName.TabIndex = 1;
+            this.TeamName.Text = "Team Name";
+            // 
+            // lsbPlayerInTeam
+            // 
+            this.lsbPlayerInTeam.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lsbPlayerInTeam.FormattingEnabled = true;
+            this.lsbPlayerInTeam.ItemHeight = 16;
+            this.lsbPlayerInTeam.Location = new System.Drawing.Point(91, 145);
+            this.lsbPlayerInTeam.Name = "lsbPlayerInTeam";
+            this.lsbPlayerInTeam.Size = new System.Drawing.Size(234, 484);
+            this.lsbPlayerInTeam.TabIndex = 2;
+            // 
+            // txtTeamName
+            // 
+            this.txtTeamName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTeamName.Location = new System.Drawing.Point(440, 119);
+            this.txtTeamName.Name = "txtTeamName";
+            this.txtTeamName.Size = new System.Drawing.Size(204, 22);
+            this.txtTeamName.TabIndex = 3;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirstName.Location = new System.Drawing.Point(440, 251);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(204, 22);
+            this.txtFirstName.TabIndex = 4;
+            // 
+            // CreatePlayer
+            // 
+            this.CreatePlayer.AutoSize = true;
+            this.CreatePlayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CreatePlayer.Location = new System.Drawing.Point(471, 192);
+            this.CreatePlayer.Name = "CreatePlayer";
+            this.CreatePlayer.Size = new System.Drawing.Size(156, 25);
+            this.CreatePlayer.TabIndex = 5;
+            this.CreatePlayer.Text = "Create Player";
+            // 
+            // FirstName
+            // 
+            this.FirstName.AutoSize = true;
+            this.FirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FirstName.Location = new System.Drawing.Point(436, 228);
+            this.FirstName.Name = "FirstName";
+            this.FirstName.Size = new System.Drawing.Size(96, 20);
+            this.FirstName.TabIndex = 6;
+            this.FirstName.Text = "First Name";
+            // 
+            // LastName
+            // 
+            this.LastName.AutoSize = true;
+            this.LastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LastName.Location = new System.Drawing.Point(436, 291);
+            this.LastName.Name = "LastName";
+            this.LastName.Size = new System.Drawing.Size(95, 20);
+            this.LastName.TabIndex = 7;
+            this.LastName.Text = "Last Name";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLastName.Location = new System.Drawing.Point(440, 314);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(204, 22);
+            this.txtLastName.TabIndex = 8;
+            // 
+            // btnCreatePlayer
+            // 
+            this.btnCreatePlayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreatePlayer.Location = new System.Drawing.Point(440, 342);
+            this.btnCreatePlayer.Name = "btnCreatePlayer";
+            this.btnCreatePlayer.Size = new System.Drawing.Size(204, 48);
+            this.btnCreatePlayer.TabIndex = 9;
+            this.btnCreatePlayer.Text = "Create Player";
+            this.btnCreatePlayer.UseVisualStyleBackColor = true;
+            this.btnCreatePlayer.Click += new System.EventHandler(this.btnCreatePlayer_Click);
+            // 
+            // btnAddToTeam
+            // 
+            this.btnAddToTeam.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddToTeam.Location = new System.Drawing.Point(440, 487);
+            this.btnAddToTeam.Name = "btnAddToTeam";
+            this.btnAddToTeam.Size = new System.Drawing.Size(204, 48);
+            this.btnAddToTeam.TabIndex = 10;
+            this.btnAddToTeam.Text = "Add To Team";
+            this.btnAddToTeam.UseVisualStyleBackColor = true;
+            this.btnAddToTeam.Click += new System.EventHandler(this.btnAddToTeam_Click);
+            // 
+            // cmbPlayers
+            // 
+            this.cmbPlayers.FormattingEnabled = true;
+            this.cmbPlayers.Location = new System.Drawing.Point(440, 460);
+            this.cmbPlayers.Name = "cmbPlayers";
+            this.cmbPlayers.Size = new System.Drawing.Size(204, 21);
+            this.cmbPlayers.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(440, 583);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(204, 48);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Create Team";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // TeamMembers
+            // 
+            this.TeamMembers.AutoSize = true;
+            this.TeamMembers.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TeamMembers.Location = new System.Drawing.Point(87, 119);
+            this.TeamMembers.Name = "TeamMembers";
+            this.TeamMembers.Size = new System.Drawing.Size(131, 20);
+            this.TeamMembers.TabIndex = 14;
+            this.TeamMembers.Text = "Team Members";
+            // 
+            // ChoosePlayer
+            // 
+            this.ChoosePlayer.AutoSize = true;
+            this.ChoosePlayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ChoosePlayer.Location = new System.Drawing.Point(461, 432);
+            this.ChoosePlayer.Name = "ChoosePlayer";
+            this.ChoosePlayer.Size = new System.Drawing.Size(166, 25);
+            this.ChoosePlayer.TabIndex = 15;
+            this.ChoosePlayer.Text = "Choose Player";
+            // 
+            // formTeamCreator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(726, 684);
+            this.Controls.Add(this.ChoosePlayer);
+            this.Controls.Add(this.TeamMembers);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmbPlayers);
+            this.Controls.Add(this.btnAddToTeam);
+            this.Controls.Add(this.btnCreatePlayer);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.LastName);
+            this.Controls.Add(this.FirstName);
+            this.Controls.Add(this.CreatePlayer);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.txtTeamName);
+            this.Controls.Add(this.lsbPlayerInTeam);
+            this.Controls.Add(this.TeamName);
+            this.Controls.Add(this.TeamCreator);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "formTeamCreator";
+            this.ShowIcon = false;
+            this.Text = "Team Creator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TeamCreator;
+        private System.Windows.Forms.Label TeamName;
+        private System.Windows.Forms.ListBox lsbPlayerInTeam;
+        private System.Windows.Forms.TextBox txtTeamName;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Label CreatePlayer;
+        private System.Windows.Forms.Label FirstName;
+        private System.Windows.Forms.Label LastName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Button btnCreatePlayer;
+        private System.Windows.Forms.Button btnAddToTeam;
+        private System.Windows.Forms.ComboBox cmbPlayers;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label TeamMembers;
+        private System.Windows.Forms.Label ChoosePlayer;
+    }
+}
+
---
+++ TurneringsPlan/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TurneringsPlan
+{
+    public partial class formTeamCreator : Form
+    {
+        public formTeamCreator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreatePlayer_Click(object sender, EventArgs e)
+        {
+            if (ValidName()) 
+            { 
+                cmbPlayers.Items.Add(txtFirstName.Text + " " + txtLastName.Text);
+            }
+
+            else
+            {
+                MessageBox.Show("Please fill out the player's name.");
+            }
+            
+        }
+
+        private void btnAddToTeam_Click(object sender, EventArgs e)
+        {
+            lsbPlayerInTeam.Items.Add(cmbPlayers.SelectedItem);
+        }
+
+        public bool ValidName()
+        {
+            if (txtFirstName.Text == "")
+                return false;
+            if (txtLastName.Text == "")
+                return false;
+            return true;
+        }
+    }
+}
