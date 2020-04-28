9910f14690d281cb1aba1c1ee9a60bd40f280209
Victor Hoffmann Risager <vict2464@elev.aatg.dk>
2020-04-28 16:14:01 +0200

    Added first characters of the name to uppercase, and added comments to everything.

--- TurneringsPlan/Form1.cs
+++ TurneringsPlan/Form1.cs
@@ -12,18 +12,36 @@ namespace TurneringsPlan
 {
     public partial class formTeamCreator : Form
     {
+        /// <summary>
+        /// Temporary storage for player name.
+        /// </summary>
+        public string tempName;
         public formTeamCreator()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Represents button generate player clicked.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
         private void btnCreatePlayer_Click(object sender, EventArgs e)
         {
+            //stores the name of the player temporarily, and makes the first letter uppercase.
+            tempName = FirstLetterToUpper(txtFirstName.Text) + " " + FirstLetterToUpper(txtLastName.Text); 
+
+            //Checks if the name is valid. 
             if (ValidName()) 
-            { 
-                cmbPlayers.Items.Add(txtFirstName.Text + " " + txtLastName.Text);
+            {
+                //adds the name to the combobox, and resets the textboxes.
+                cmbPlayers.Items.Add(tempName);
+                txtFirstName.Text = "";
+                txtLastName.Text = "";
             }
 
+            //if not valid, show error messange
             else
             {
                 MessageBox.Show("Please fill out the player's name.");
@@ -31,18 +49,75 @@ namespace TurneringsPlan
             
         }
 
+        /// <summary>
+        /// Represents button Add to team clicked.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnAddToTeam_Click(object sender, EventArgs e)
         {
-            lsbPlayerInTeam.Items.Add(cmbPlayers.SelectedItem);
+            //checks if the player is already in the team.
+            if (ValidPlayer())
+            {
+                //adds the player to the team.
+                lsbPlayerInTeam.Items.Add(cmbPlayers.SelectedItem);
+            }
+            //else show errormesagge.
+            else
+            {
+                MessageBox.Show("The player is already in the team.");
+            }
+            
         }
 
-        public bool ValidName()
+        /// <summary>
+        /// This checks if the player already exists in the team.
+        /// </summary>
+        /// <returns> a boolean </returns>
+        private bool ValidPlayer()
         {
-            if (txtFirstName.Text == "")
-                return false;
-            if (txtLastName.Text == "")
+            if (lsbPlayerInTeam.Items.Contains(cmbPlayers.SelectedItem))
                 return false;
             return true;
         }
+
+        /// <summary>
+        /// Checks if the typed name is valid. 
+        /// </summary>
+        /// <returns> a boolean </returns>
+        private bool ValidName()
+        {
+            //if firstname is empty.
+            if (txtFirstName.Text == "")
+                return false;
+            //if last name is empty.
+            if (txtLastName.Text == "")
+                return false;
+            //if the list of players already contains the name. 
+            if (cmbPlayers.Items.Contains(tempName))
+                return false;
+            //else return true.
+            return true;
+        }
+
+        /// <summary>
+        /// Converts the first character of a string to uppercase.
+        /// </summary>
+        /// <param name="input"> represents the string input. </param>
+        /// <returns> string </returns>
+        public string FirstLetterToUpper(string input)
+        {
+            //if the textbox is empty return null. 
+            if (input == null)
+                return null;
+
+            //if the input contains any characters.
+            if (input.Length > 1)
+                //return the first char to upper.
+                return char.ToUpper(input[0]) + input.Substring(1);
+            //return the character to upper.
+            return input.ToUpper();
+        }
+
     }
 }
